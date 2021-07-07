768c0d12a120481054921b1b786b8932cc9b216b
2293736867 <2293736867@qq.com>
2021-07-07 14:47:24 +0800

    ShardingSphere data sharding and read write splitting

---
+++ ShardingSphereMyBatisPlusDataShardingAndReadWriteSplitting/demo/src/main/kotlin/com/example/demo/IdGenerator.kt
@@ -0,0 +1,21 @@
+package com.example.demo
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator
+import com.github.yitter.contract.IdGeneratorOptions
+import com.github.yitter.idgen.YitIdHelper
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class IdGenerator:IdentifierGenerator {
+    val options = IdGeneratorOptions(1)
+
+    @PostConstruct
+    fun init(){
+        YitIdHelper.setIdGenerator(options)
+    }
+
+    override fun nextId(entity: Any?):Long?{
+        return YitIdHelper.nextId()
+    }
+}
