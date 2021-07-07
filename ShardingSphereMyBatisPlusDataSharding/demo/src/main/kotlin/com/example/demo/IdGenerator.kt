2c9bf42c2a1a672c6c56225f76b28ac4b0f1a746
2293736867 <2293736867@qq.com>
2021-07-07 14:43:17 +0800

    remove unnecessary dependency

--- ShardingSphereMyBatisPlusDataSharding/demo/src/main/kotlin/com/example/demo/IdGenerator.kt
+++ ShardingSphereMyBatisPlusDataSharding/demo/src/main/kotlin/com/example/demo/IdGenerator.kt
@@ -3,7 +3,6 @@ package com.example.demo
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator
 import com.github.yitter.contract.IdGeneratorOptions
 import com.github.yitter.idgen.YitIdHelper
-import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
