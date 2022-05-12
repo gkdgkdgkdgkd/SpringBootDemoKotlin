1ee0e695add2a90aed85322c1d575702c0b6dedf
gkdgkdgkdgkd <4tixf3eb@anonaddy.me>
2022-05-12 16:07:26 +0800

    Update Spring Boot 2.6.7 and Kotlin 1.6.21

--- Docker/README.md
+++ Docker/README.md
@@ -1,3 +1,14 @@
-# `Docker`部署`Demo`
+# 内容
 
-注意服务器架构为`aarch64`而不是常见的`x86`，请注意对应修改`Dockerfile`。
+`Docker`部署。`build`文件夹包含了`JAR`以及`Dockerfile`，直接在该文件夹下执行`docker build`即可。
+
+# 环境
+
+- `Docker 20.10.14`
+- `OpenJDK 17`镜像
+
+# 博客
+
+[地址](https://blog.csdn.net/qq_27525611/article/details/108571663)
+
+注意博客中的服务器已经不再使用，因此`Dockerfile`中的`JDK`改为`x86`的`OpenJDK 17.0.2`。
--- Docker/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ Docker/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,16 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@RestController
-class DemoApplication{
-    @GetMapping("/test")
-    fun test() = "success"
-}
+class DemoApplication
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+	runApplication<DemoApplication>(*args)
 }
